463670ae66657a44f07216419d04a794bca4a365
Alexandra <alex.viktorini@gmail.com>
2025-10-31 00:33:05 +0100

    fix fefe

--- src/app/api/auth/[...nextauth]/authOptions.ts
+++ src/app/api/auth/[...nextauth]/authOptions.ts
@@ -1,17 +1,18 @@
 import NextAuth from 'next-auth';
 import GoogleProvider from 'next-auth/providers/google';
 
+
 export const authOptions = {
  providers: [
-  GoogleProvider({
-   clientId: process.env.GOOGLE_ID,
-   clientSecret: process.env.GOOGLE_SECRET,
-  }),
+    GoogleProvider({ //umožní prihlasovanie skrz Google
+      clientId: process.env.GOOGLE_CLIENT_ID || "",
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET || "",
+    }),
  ],
  secret: process.env.NEXTAUTH_SECRET,
  pages: {
-    signIn: '/auth/prihlasenie',
-    signOut: '/auth/odhlasenie',
+    signIn: '/sign-in',
+    signOut: '/sign-out',
   },
   callbacks: {
     async redirect({ url, baseUrl }: { url: string; baseUrl: string }) {
