db47ed31974035eacac330a694f861bbb2ea0940
Alexandra <alex.viktorini@gmail.com>
2025-10-31 14:29:44 +0100

    testing

--- src/app/components/SideBar.tsx
+++ src/app/components/SideBar.tsx
@@ -9,8 +9,8 @@ export default async function SideBar(){
     const thisUserId = cookieStore.get("session-user-id")
     //
     //const joinedChannels = await db`SELECT channels.id, channels.name, channels.icon FROM channels LEFT JOIN in-channel ON in-channel.channel_id = channels.id WHERE in-channel.user_id = ${thisUserId} ASC`
-    const joinedChannels = await db`SELECT id, name, desc, category, icon FROM channels SORT BY members DESC`
-    const joinedGroups = await db`SELECT id, name, desc, category, icon FROM channels SORT BY members DESC LIMIT 8`
+    //const joinedChannels = await db`SELECT id, name, desc, category, icon FROM channels SORT BY members DESC`
+    //const joinedGroups = await db`SELECT id, name, desc, category, icon FROM channels SORT BY members DESC LIMIT 8`
 
 
 if (await thisUserId || null){
@@ -20,33 +20,6 @@ if (await thisUserId || null){
                 <ul>
                     <li>
                         <p>Joined channels</p>
-                        {joinedChannels.map ((c) =>
-                        <div key={c.id}>
-                            <Link href={`/channel/${c.id}`}>
-                                {c.icon != null && c.icon.length > 0 ? (
-                                    <Image src={c.icon} alt="" />
-                                ) : null}
-                                {c.name != null ? (
-                                    <p>{c.name}</p>
-                                ) : null}
-                             </Link>
-                        </div>
-                        )}
-                    </li>
-                    <li>
-                        <p>Joined groups</p>
-                        {joinedGroups.map ((g) =>
-                        <div key={g.id}>
-                            <Link href={`/channel/${g.id}`}>
-                                {g.icon != null && g.icon.length > 0 ? (
-                                    <Image src={g.icon} alt="" />
-                                ) : null}
-                                {g.name != null ? (
-                                    <p>{g.name}</p>
-                                ) : null}
-                             </Link>
-                        </div>
-                        )}
                     </li>
                 </ul>
             </div>
