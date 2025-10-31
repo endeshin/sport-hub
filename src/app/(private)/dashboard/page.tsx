e26410beb34113791c4e0dd613d1ca602f3ee92f
Alexandra <alex.viktorini@gmail.com>
2025-10-31 15:26:33 +0100

    fix 0.3.7

--- src/app/(private)/dashboard/page.tsx
+++ src/app/(private)/dashboard/page.tsx
@@ -6,7 +6,7 @@ import Image from "next/image";
 
 export default async function dashboardPage() {
     const db = createDB();
-    const channelNames = await db`SELECT id, name, dec, icon FROM channels ORDER BY members LIMIT 8 DESC`
+    const channelNames = await db`SELECT id, name, dec, icon FROM channels ORDER BY members LIMIT 8`
 
     return(
         <div>
